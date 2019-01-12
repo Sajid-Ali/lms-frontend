9d9a239898d61bbc9f06a2421e7af2043d25ded4
haider-nayyar <haidernayyar@gmail.com>
2019-01-13 02:38:14 +0500

    test with console

--- src/app/Services/token.service.ts
+++ src/app/Services/token.service.ts
@@ -1,69 +1,73 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TokenService {
 
-  private iss = {
-    login: 'https://elearn-fyp2.herokuapp.com/api/login',
-    signup: 'https://elearn-fyp2.herokuapp.com/api/signup'
-  };
+    private iss = {
+        login: 'https://elearn-fyp2.herokuapp.com/api/login',
+        signup: 'https://elearn-fyp2.herokuapp.com/api/signup'
+    };
 
 
-  constructor() { }
+    constructor() {
+    }
 
-  tokenHandler(token) {
-    this.set(token);
-    console.log(this.isValid());
-  }
+    tokenHandler(token) {
+        this.set(token);
+        console.log(this.isValid());
+    }
 
-  setUserTokenHandler(userToken) {
-    localStorage.setItem('authUser', JSON.stringify(userToken));
-  }
+    setUserTokenHandler(userToken) {
+        localStorage.setItem('authUser', JSON.stringify(userToken));
+    }
 
-  getUserTokenHandler() {
-    return JSON.parse(localStorage.getItem('authUser'));
-  }
+    getUserTokenHandler() {
+        return JSON.parse(localStorage.getItem('authUser'));
+    }
 
-  loggedIn() {
+    loggedIn() {
 
-    return this.isValid();
-  }
+        return this.isValid();
+    }
 
-  set(token) {
-    localStorage.setItem('token', token);
-  }
-  get(token) {
-    return localStorage.getItem(token);
-  }
-  remove() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('authUser');
-  }
+    set(token) {
+        localStorage.setItem('token', token);
+    }
 
-  isValid() {
-   const token = this.get('token');
-   if (token) {
-     const payload = this.payload(token);
-     if (payload) {
-       return (Object.values(this.iss).indexOf(payload.iss) > -1) ? true : false;
-     }
-   }
+    get(token) {
+        return localStorage.getItem(token);
+    }
 
-   return false;
+    remove() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('authUser');
+    }
 
-  }
+    isValid() {
+        const token = this.get('token');
+        console.log('Getting token******', token);
+        if (token) {
+            const payload = this.payload(token);
+            if (payload) {
+                return (Object.values(this.iss).indexOf(payload.iss) > -1) ? true : false;
+            }
+        }
 
-  payload(token) {
-    const payload = token.split('.')[1];
-    return this.decode(payload);
-  }
+        return false;
 
-  decode(payload) {
-    return JSON.parse(atob(payload));
-  }
+    }
+
+    payload(token) {
+        const payload = token.split('.')[1];
+        return this.decode(payload);
+    }
+
+    decode(payload) {
+        return JSON.parse(atob(payload));
+    }
 
 
 }
